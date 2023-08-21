543041468783f6e779b64d9e99fa254f178ceb83
damiga14 <davidamiga14@gmail.com>
2023-08-21 15:13:50 -0600

    levana nuevo listo

--- src/pages/Tab5.jsx
+++ src/pages/Tab5.jsx
@@ -8,18 +8,18 @@ import Levana from '../components/Levana'
 let months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 
 function Tab5() {
-  let [cuantas, setCuantas] = useState([])
+  // let [cuantas, setCuantas] = useState([])
   let [levanaResponse2, setLevanaResponse2] = useState([])
-  let [loading, setLoading] = useState(true)
+  // let [loading, setLoading] = useState(true)
 
   function levanaCall() {
     axios.get(`${process.env.REACT_APP_BackURL}/api/v1/getLevana`)
       .then((response) => {
         setLevanaResponse2(response.data.data[0].json)
-        setLoading(false)
+        // setLoading(false)
 
-        if (response.data.data[0].json.molad.length == 13) { setCuantas([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) }
-        else { setCuantas([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) }
+        // if (response.data.data[0].json.molad.length == 13) { setCuantas([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) }
+        // else { setCuantas([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) }
       })
       .catch((err) => { console.log(err) })
   }
@@ -48,7 +48,7 @@ function Tab5() {
             <IonText>Fechas y horarios de Bircat Halebana para el año en curso</IonText>
           </div>
 
-          {loading ? <Loading /> : null}
+          {/* {loading ? <Loading /> : null} */}
 
           {/* {cuantas.map((i) => { return <><Levana month={i} key={i} /></> })} */}
 
