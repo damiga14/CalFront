b8396db12797ffaae348e9eca939559600f5972b
David Amiga <49048340+damiga14@users.noreply.github.com>
2024-03-01 14:55:46 -0600

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,8 +1,70 @@
 /*
 
+purim katan agregar q no se dice ana
+
 januka 2023 el dia q cayo rosh jodesh no salia halel conoleto
 
 tambien el dia del ayuno de diciembre, cayo en viernes, no salia hora q acaba ayuno 
+
+
+
+
+El 21 de marzo está escrito que en la noche es lectura de la meguila, error, la lectura es Motzae Shabat 23 de marzo y domingo 24 de marzo
+
+Tienes que poner Domingo 24 de marzo Purim- lectura de la meguila Al Hanisim no se dice
+Anna tajanun)
+
+Martes 9 de Abril poner beraja de los árboles
+
+1 de Mayo hay que poner Isru Jag
+
+Martes 11 de junio poner en la noche desvelada
+
+Miércoles 12 de junio subir shekia y en encendido poner también Rabenu Tam 8:27 pm
+
+El 13 de junio falta también R tam
+
+Viernes 14 de junio poner Isru Jag
+
+Martes 23 de Abril subir shekia y después de encendido agregar R tam 8:09 pm
+
+24 de Abril agregar R tam
+
+domingo 28 de Abril poner víspera de yom tob
+
+Lunes 29 de Abril subir shekia y agregar R tam y martes 30 de Abril agregar R tam
+
+12 de agosto agrandar y en rojo víspera de 9 ab, agregar 1956 años del jorban b.Hamikdash
+
+Jueves 22 de Agosto agregar en la noche Hatarat Nedarim
+
+Me gustaría que las cosas importantes se pongan en rojo como por ejemplo el 2 de Elul primer día de selijot sefaradim
+
+Tu beab- 9 de Ab -ayuno 17 de Tamuz
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 shabat q quede dentro del cuadro
 
